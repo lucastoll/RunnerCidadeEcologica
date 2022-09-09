f7ceddab92f62698c2c3bd116bec8e9f94f3e55f
Lucas Fernandes Tolotto <86172649+lucastoll@users.noreply.github.com>
2022-09-09 13:04:43 -0300

    vou trocar de pc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,38 @@
-console.log("AAAAAAAAAAAAAAAAAAA");
-const player = document.querySelector('.player');
-const obstaculocarro = document.querySelector('.obstaculocarro');
+const player = document.querySelector(".player");
+const obstaculocarro = document.querySelector(".obstaculocarro");
 
 var aux = 0;
-/*Define a ação para qd ele teclar a tecla w*/
+
+/* Define a ação para quando ele teclar a tecla W */
+
 const pular = () => {
-    player.classList.add('pula');
-    setTimeout(()=>{
-        player.classList.remove('pula');
-        aux=0;
-    }, 1000);
-}
-document.addEventListener('keydown', (event) => {
-    var tecla = event.key;
-    var evento = event.code;
-    
-    if(tecla ==='w' && aux===0)
-    {
-        aux=1;
-        pular();
-    }
-})
-/*Define a colisão e condição de parada*/
+  player.classList.add("pula");
+  setTimeout(() => {
+    player.classList.remove("pula");
+    aux = 0;
+  }, 1000);
+};
+
+document.addEventListener("keydown", (event) => {
+  var tecla = event.key;
+
+  if (tecla === "w" && aux === 0) {
+    aux = 1;
+    pular();
+  }
+});
+
+/* Define a colisão e condição de parada */
+
 const intervalo = setInterval(() => {
-    const posicaocarro = obstaculocarro.offsetLeft;
-    const posicaoplayer = window.getComputedStyle(player).bottom.replace('px','');
-    console.log(posicaoplayer);
-if(posicaocarro <= 97 && posicaocarro>0 && posicaoplayer < 50)
-{
-    obstaculocarro.style.animation = 'none';
+  const posicaocarro = obstaculocarro.offsetLeft;
+  const posicaoplayer = window.getComputedStyle(player).bottom.replace("px", "");
+
+  console.log(posicaoplayer);
+  if (posicaocarro <= 97 && posicaocarro > 0 && posicaoplayer < 50) {
+    obstaculocarro.style.animation = "none";
     obstaculocarro.style.left = `${posicaocarro}px`;
-    player.style.animation = 'none';
-    player.style.bottom = `${posicaoplayer}`;
-}
-}, 10);
+    player.style.animation = "none";
+    player.style.bottom = `${posicaoplayer}px`;
+  }
+}, 1);
