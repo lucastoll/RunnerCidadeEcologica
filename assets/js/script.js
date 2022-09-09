f68a2ba4d0f0f876c7166fa3dc7f67f96b99d9a7
lucastoll <lucasfernandestolotto@gmail.com>
2022-09-09 14:01:31 -0300

    botao start funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 const player = document.querySelector(".player");
-const obstaculocarro = document.querySelector(".obstaculocarro");
+const obstaculo1 = document.querySelector(".obstaculocarro");
+const buttonStart = document.getElementById("buttonStart");
 
-var aux = 0;
+var auxPulo = 0;
 
 /* Define a ação para quando ele teclar a tecla W */
 
@@ -9,30 +10,49 @@ const pular = () => {
   player.classList.add("pula");
   setTimeout(() => {
     player.classList.remove("pula");
-    aux = 0;
+    auxPulo = 0;
   }, 1000);
 };
 
 document.addEventListener("keydown", (event) => {
   var tecla = event.key;
+  console.log(auxPulo)
 
-  if (tecla === "w" && aux === 0) {
-    aux = 1;
+  if (tecla === "w" && auxPulo === 0) {
+    auxPulo = 1;
     pular();
   }
 });
 
 /* Define a colisão e condição de parada */
+let intervalo
 
-const intervalo = setInterval(() => {
-  const posicaocarro = obstaculocarro.offsetLeft;
+buttonStart.addEventListener("click", () => {
+  obstaculo1.style.left = "auto";
+  obstaculo1.style.right = "-5%";
+  obstaculo1.style.animation = "carroparado linear infinite 2s";
+  console.log("a")
+
+  auxPulo = 0;
+  player.style.bottom = '0px';
+  player.classList.remove("pula");
+  intervalo = setInterval(colisao, 10);
+})
+
+
+function colisao(){
+  const posicaocarro = obstaculo1.offsetLeft;
   const posicaoplayer = window.getComputedStyle(player).bottom.replace("px", "");
 
-  console.log(posicaoplayer);
+  console.log(`Posição obstaculo = ${posicaocarro}, posicao player = ${posicaoplayer}`);
   if (posicaocarro <= 97 && posicaocarro > 0 && posicaoplayer < 50) {
-    obstaculocarro.style.animation = "none";
-    obstaculocarro.style.left = `${posicaocarro}px`;
+    obstaculo1.style.animation = "none";
+    obstaculo1.style.left = `${posicaocarro}px`;
     player.style.animation = "none";
+    player.classList.remove("pula");
     player.style.bottom = `${posicaoplayer}px`;
+    auxPulo = 1;
+    
+    clearInterval(intervalo);
   }
-}, 1);
+}
