cb9d2e1d0973ee7c1718149e1f34ca7474d3edf5
Lucas Fernandes Tolotto <86172649+lucastoll@users.noreply.github.com>
2022-11-17 13:00:12 -0300

    first commit

---
+++ forge.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  config: {
+    forge: {
+        packagerConfig: {
+            icon: '/assets/img/sustentabilidade.ico'
+        }
+    }
+  },
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
